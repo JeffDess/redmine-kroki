436bc57040540a11730068dd4ebf5cbb1d0d8109
Jean-François Dessureault <jfdess@gmail.com>
2024-06-05 23:11:59 -0400

    feat: display converted images in macro

---
+++ src/app/helpers/redmine_kroki_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module RedmineKrokiHelper
+  require 'net/http'
+  require 'uri'
+
+  def send_kroki_request(diagram_type, diagram_content)
+    endpoint = Setting.plugin_redmine_kroki['kroki_endpoint']
+    url = URI.parse("#{endpoint}/#{diagram_type}/svg")
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Post.new(url.path)
+
+    request.content_type = 'text/plain'
+    request.body = diagram_content
+
+    response = http.request(request)
+
+    response.body
+  end
+end
