13afec7eb1c737d00ee19e38001f49c8973d7956
Jean-François Dessureault <jfdess@gmail.com>
2024-06-05 21:32:51 -0400

    feat: add endpoint settings

--- src/init.rb
+++ src/init.rb
@@ -9,10 +9,15 @@ Redmine::Plugin.register :redmine_kroki do
   author_url 'https://github.com/jeffdess'
 
   settings default: {
-             'kroki_bpmn' => true,
-             'kroki_excalidraw' => true,
-             'kroki_mermaid' => true,
-             'kroki_diagramsnet' => true
+             'kroki_endpoint' => 'http://kroki',
+             'kroki_bpmn_endpoint' => 'http://bpmn',
+             'kroki_bpmn_enabled' => true,
+             'kroki_excalidraw_endpoint' => 'http://excalidraw',
+             'kroki_excalidraw_enabled' => true,
+             'kroki_mermaid_endpoint' => 'http://mermaid',
+             'kroki_mermaid_enabled' => true,
+             'kroki_diagramsnet_endpoint' => 'http://diagramsnet',
+             'kroki_diagramsnet_enabled' => true
            },
            partial: 'settings/redmine-kroki_settings'
 
