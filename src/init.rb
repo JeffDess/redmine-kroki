ed7a76e96190b4f88ca4d72f7c9fdb6e25867898
Jean-François Dessureault <jfdess@gmail.com>
2024-06-16 21:27:01 -0400

    feat: add auto dark mode + settings

--- src/init.rb
+++ src/init.rb
@@ -8,7 +8,11 @@ Redmine::Plugin.register :redmine_kroki do
   url 'https://github.com/jeffdess/redmine-kroki'
   author_url 'https://github.com/jeffdess'
 
-  settings default: { 'kroki_url' => 'http://kroki:8000' },
+  settings default: {
+             kroki_url: 'http://kroki:8000',
+             dark_themes: %w[dark dark-theme redmine-dark],
+             force_dark: false
+           },
            partial: 'settings/redmine-kroki_settings'
 
   Redmine::WikiFormatting::Macros.register do
@@ -19,10 +23,16 @@ Redmine::Plugin.register :redmine_kroki do
          "{{kroki(mermaid, theme=dark)\nflowchart LR\n  Hello --> World\n}}"
     macro :kroki do |_obj, args, text|
       extend RedmineKrokiHelper
-      kroki_url = Setting.plugin_redmine_kroki['kroki_url']
+      kroki_url = Setting.plugin_redmine_kroki[:kroki_url]
       diagram_type = args.shift
       diagram_options = parse_macro_options(args)
-      res = convert_diagram(kroki_url, diagram_type, text, diagram_options)
+      diagram = convert_diagram(kroki_url, diagram_type, text, diagram_options)
+      css = css_class(
+        Setting.plugin_redmine_kroki[:force_dark],
+        Setting.plugin_redmine_kroki[:dark_themes],
+        User.current.preference.theme
+      )
+      res = wrap_diagram(diagram, css)
 
       res.html_safe
     end
