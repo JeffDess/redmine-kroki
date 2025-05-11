91f3abd6e63b766e5102c079a616088435972436
Jean-François Dessureault <jfdess@gmail.com>
2025-05-10 22:30:19 -0400

    chore: bump v0.4.0

--- src/init.rb
+++ src/init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_kroki do
   name 'Redmine Kroki Plugin'
   author 'Jean-François Dessureault'
   description 'Render graphs in Redmine issues and wiki with Kroki'
-  version '0.3.5'
+  version '0.4.0'
   url 'https://github.com/jeffdess/redmine-kroki'
   author_url 'https://github.com/jeffdess'
 
