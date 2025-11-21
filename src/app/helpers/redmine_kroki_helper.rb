ce78963eaf7adbdfd5df320b81ff354434f8cc55
Jean-François Dessureault <jfdessureault@arcanetech.io>
2025-11-21 08:43:05 -0500

    chore: refactor RedmineKrokiHelper to include all of its functions

--- src/app/helpers/redmine_kroki_helper.rb
+++ src/app/helpers/redmine_kroki_helper.rb
@@ -19,6 +19,36 @@ module RedmineKrokiHelper
     res.body
   end
 
+  def parse_macro_options(options)
+    return nil if options.empty?
+
+    options.map { |option| option.split('=') }.to_h.transform_keys(&:to_sym)
+  end
+
+  def wrap_diagram(diagram, classes, options, diagram_type = nil)
+    version = Redmine::Plugin.registered_plugins[:redmine_kroki].version
+    style_tag = generate_css_tag('redmine-kroki')
+    style_tag = style_tag + embed_fonts(diagram_type)
+    styles = convert_options_to_style(options)
+    classes = classes.dup << ' resized' unless styles.empty?
+    classes = classes.dup << " #{diagram_type.downcase.delete(' -')}" if diagram_type
+
+    "#{style_tag}<div class=\"#{classes}\" style=\"#{styles}\">#{diagram}</div>"
+  end
+
+  def css_class(is_dark_forced, dark_themes, user_theme)
+    return 'kroki' unless user_theme && dark_themes.include?(user_theme) ||
+                          is_dark_forced
+
+    'kroki dark'
+  end
+
+  def get_user_theme(user)
+    user.preference.respond_to?('theme') ? user.preference.theme : nil
+  end
+
+  private
+
   def send_kroki_request(kroki_url, diagram_type, diagram_content, diagram_options = nil)
     type = diagram_type.downcase.delete(' -')
     url = URI.parse("#{kroki_url}/#{type}/svg")
@@ -32,86 +62,58 @@ module RedmineKrokiHelper
 
     http.request(req)
   end
-end
 
-def sanitize_diagram(diagram_type, diagram_content)
-  return diagram_content.tr("\r", "\n") \
-    if diagram_type == 'nomnoml' && !diagram_content.nil?
+  def sanitize_diagram(diagram_type, diagram_content)
+    return diagram_content.tr("\r", "\n") \
+      if diagram_type == 'nomnoml' && !diagram_content.nil?
 
-  diagram_content
-end
-
-def add_diagram_options(request, options)
-  options.each do |k, v|
-    request.add_field("Kroki-Diagram-Options-#{k.to_s.downcase.capitalize}", v) \
-      unless PLUGIN_OPTIONS.include?(k)
-  end
-end
-
-def parse_macro_options(options)
-  return nil if options.empty?
-
-  options.map { |option| option.split('=') }.to_h.transform_keys(&:to_sym)
-end
-
-def embed_fonts(diagram_type)
-  return '' unless diagram_type
-
-  case diagram_type.downcase
-  when 'excalidraw'
-    generate_css_tag('excalidraw')
-  else
-    ''.html_safe
-  end
-end
-
-def wrap_diagram(diagram, classes, options, diagram_type = nil)
-  version = Redmine::Plugin.registered_plugins[:redmine_kroki].version
-  style_tag = generate_css_tag('redmine-kroki')
-  style_tag = style_tag + embed_fonts(diagram_type)
-  styles = convert_options_to_style(options)
-  classes = classes.dup << ' resized' unless styles.empty?
-  classes = classes.dup << " #{diagram_type.downcase.delete(' -')}" if diagram_type
-
-  "#{style_tag}<div class=\"#{classes}\" style=\"#{styles}\">#{diagram}</div>"
-end
-
-def only_digits?(value)
-  !!value.match?(/\A\d+\z/)
-end
-
-def convert_options_to_style(options)
-  return '' if options.nil?
-
-  plugin_options = options.select do |k, v|
-    PLUGIN_OPTIONS.include?(k) && only_digits?(v)
+    diagram_content
   end
 
-  return '' if plugin_options.empty?
-
-  style = ''.dup
-  plugin_options.reduce(style) do |acc, (k, v)|
-    acc << "#{plugin_option_css_rule(k)}: #{v}px; "
+  def add_diagram_options(request, options)
+    options.each do |k, v|
+      request.add_field("Kroki-Diagram-Options-#{k.to_s.downcase.capitalize}", v) \
+        unless PLUGIN_OPTIONS.include?(k)
+    end
   end
 
-  style.strip
-end
+  def embed_fonts(diagram_type)
+    return '' unless diagram_type
 
-def plugin_option_css_rule(key)
-  key == :max_width ? key.to_s.gsub('_', '-') : key.to_s.split('_').last
-end
+    case diagram_type.downcase
+    when 'excalidraw'
+      generate_css_tag('excalidraw')
+    else
+      ''.html_safe
+    end
+  end
 
-def css_class(is_dark_forced, dark_themes, user_theme)
-  return 'kroki' unless user_theme && dark_themes.include?(user_theme) ||
-                        is_dark_forced
+  def only_digits?(value)
+    !!value.match?(/\A\d+\z/)
+  end
 
-  'kroki dark'
-end
+  def convert_options_to_style(options)
+    return '' if options.nil?
 
-def get_user_theme(user)
-  user.preference.respond_to?('theme') ? user.preference.theme : nil
-end
+    plugin_options = options.select do |k, v|
+      PLUGIN_OPTIONS.include?(k) && only_digits?(v)
+    end
 
-def generate_css_tag (filename)
-  stylesheet_link_tag filename, plugin: "redmine_kroki"
+    return '' if plugin_options.empty?
+
+    style = ''.dup
+    plugin_options.reduce(style) do |acc, (k, v)|
+      acc << "#{plugin_option_css_rule(k)}: #{v}px; "
+    end
+
+    style.strip
+  end
+
+  def plugin_option_css_rule(key)
+    key == :max_width ? key.to_s.gsub('_', '-') : key.to_s.split('_').last
+  end
+
+  def generate_css_tag(filename)
+    stylesheet_link_tag filename, plugin: "redmine_kroki"
+  end
 end
